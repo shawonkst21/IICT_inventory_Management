812314f9f82dce9546c9ef1b8adc4ebdc1eaa045
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2026-05-03 16:06:25 +0600

    fixed isue

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__238998a8._.css",
-  "static/chunks/_c4b2e1ed._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_tsx_3363310b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_50f91daa._.js",
-  "static/chunks/_586c11e2._.js"
-],
-    source: "dynamic"
-});
